ee2bcf4603dee88dd66813cd3748778a33455bb4
andriidemchenko <demchenko.andrey@gmail.com>
2018-09-24 16:18:01 +0300

    finish create todo list

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD2NICpGj8JDMTCfFwqJ14SSpYpmJPo7vM",
+  authDomain: "ionicshopinglist-47aa3.firebaseapp.com",
+  databaseURL: "https://ionicshopinglist-47aa3.firebaseio.com",
+  projectId: "ionicshopinglist-47aa3",
+  storageBucket: "ionicshopinglist-47aa3.appspot.com",
+  messagingSenderId: "891284011850"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController} from 'ionic-angular';
+import {ShoppingListService} from "../../services/shopping-list/shopping-list.service";
+import {Observable} from 'rxjs/Observable';
+import {map} from 'rxjs/operators';
+import {Item} from "../../models/item.model";
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>;
 
+  constructor(public navCtrl: NavController,
+              private shopping: ShoppingListService) {
+    // @ts-ignore
+    this.shoppingList$ = this.shopping
+      .getShoppingList() // RETURN DB LIST
+      .snapshotChanges() //key and value
+      .pipe(map(changes => {
+        return changes.map(a => ({
+          key: a.payload.key, ...a.payload.val()
+        }));
+      }));
   }
-
 }
+
